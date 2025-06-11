b65090e1ab43320037e21229811d4a0225230829
BlumiGlass <Hand1D11@mbjcomp.org.il>
2025-06-11 11:04:39 +0300

    add controllers

---
+++ Backend/Server/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Bl.Interfaces;
+using Bl.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserServiceBl _userService;
+        public UsersController(IUserServiceBl userService,IConfiguration configuration) 
+        {
+            _userService = userService;
+        }
+        [HttpGet]
+        public ActionResult<List<UserBl>> GetAllUsers()
+        {
+            var users = _userService.GetAllUsers();
+            if (users == null || users.Count == 0)
+            {
+                return NotFound("No users found.");
+            }
+            return Ok(users);
+        }
+
+        [HttpGet("{userId}")]
+        public ActionResult<UserBl> GetUserById(int userId)
+        {
+            var user = _userService.GetUserById(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<UserBl> CreateUser([FromBody] UserBl request)
+        {
+            if (request == null || string.IsNullOrEmpty(request.Name) || string.IsNullOrEmpty(request.Phone))
+            {
+                return BadRequest("Invalid user data.");
+            }
+            var user =  _userService.CreateUser(request.Id, request.Name, request.Phone);
+            return user;
+        }
+    }
+}
--- Backend/Server/Program.cs
+++ Backend/Server/Program.cs
@@ -1,3 +1,5 @@
+using Bl;
+using Bl.Interfaces;
 using Dal.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,8 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<IBl, BlManager>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapControllers();
 
 app.Run();
