fce04e4bef59d10cea683a762749d3c67024e018
BlumiGlass <Hand1D11@mbjcomp.org.il>
2025-06-11 16:25:29 +0300

    add categories controllers

---
+++ Backend/Server/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using Bl.Interfaces;
+using Bl.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryServiceBl _categoryService;
+        public CategoriesController(IBl bl)
+        {
+            _categoryService = bl.CategoryService;
+        }
+        [HttpGet]
+        public ActionResult<List<CategoryBl>> GetAllCategories()
+        {
+            var categories =  _categoryService.GetCategories();
+            if (categories == null || categories.Count == 0)
+            {
+                return NotFound("No categories found.");
+            }
+            return Ok(categories);
+        }
+    }
+}
---
+++ Backend/Server/Controllers/SubCategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Bl.Interfaces;
+using Bl.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoriesController : ControllerBase
+    {
+        private readonly ISubCategoryServiceBl _subCategoryService;
+        public SubCategoriesController(IBl bl)
+        {
+            _subCategoryService = bl.SubCategoryService;
+        }
+        [HttpGet("{categoryId}")]
+        public ActionResult<List<SubCategoryBl>> GetSubCategoriesByCategoryId(int categoryId)
+        {
+            var subCategories = _subCategoryService.GetSubCategoriesByCategoryId(categoryId);
+            if (subCategories == null || subCategories.Count == 0)
+            {
+                return NotFound("No subcategories found for this category.");
+            }
+            return Ok(subCategories);
+        }
+
+    }
+}
